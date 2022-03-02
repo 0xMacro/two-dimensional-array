e7df90096592e911d93078c995efcab9671fd86f
Alex Sumner <alex.sumner@gmail.com>
2022-03-02 15:49:24 +0000

    added arraysAndEvents contract for tp#1

---
+++ contracts/arraysAndEvents.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.2;
+
+contract Story {
+
+    string[3] private v;
+    string[] private w;
+    string[][3] private x;
+
+    struct Voter {
+        uint weight;
+        bool voted;
+        address delegate;
+        uint vote;
+    }
+
+    Voter[] private y;
+    
+    event MsgSent(address indexed sender, string message);
+    event NoteAdded(string indexed summary, string detail);
+}
+
+
+
+
+
